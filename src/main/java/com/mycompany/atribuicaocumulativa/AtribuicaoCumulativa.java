fbe55a1209e1058e0b9e7b7f82848933dedf91b8
aluno <leodihl@gmail.com>
2024-03-07 19:27:48 -0300

    Exercício de Atribuição Cumulativa | (a += 5)

---
+++ src/main/java/com/mycompany/atribuicaocumulativa/AtribuicaoCumulativa.java
@@ -0,0 +1,17 @@
+package com.mycompany.atribuicaocumulativa;
+
+public class AtribuicaoCumulativa {
+
+    public static void main(String[] args) {
+
+      /*int a = 26;
+        System.out.println(a += 5);
+      */  
+        
+       /*int b = 26; 
+        System.out.println(b *= 2);
+      */
+    }
+}
+
+
